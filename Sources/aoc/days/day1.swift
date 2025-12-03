3599888e8420b352b7047629d7fc8ea45879e434
Taylor Plewe <tplewe@outlook.com>
2025-12-03 10:14:36 -0700

    day 1 part 2

--- Sources/aoc/days/day1.swift
+++ Sources/aoc/days/day1.swift
@@ -10,6 +10,7 @@ func day1(input: String) {
 	var pos = 50
 	var zeroes = 0
 
+	var prevPos = pos
 	for line in lines {
 		let dir: Direction = (line[line.startIndex] == "R") ? .Right : .Left
 		let amt = Int(line[line.index(after: line.startIndex)...])!
@@ -22,14 +23,26 @@ func day1(input: String) {
 		if (pos == 0) {
 			zeroes += 1
 		}
-		while (pos < 0) {
-			pos += 100
-			zeroes += 1
+		if (pos < 0) {
+			while (pos < 0) {
+				pos += 100
+				zeroes += 1
+			}
+			if pos == 0 {
+				zeroes += 1
+			}
 		}
 		while (pos >= 100) {
 			pos -= 100
 			zeroes += 1
 		}
+
+		// edge case
+		if prevPos == 0 && dir == .Left {
+			zeroes -= 1
+		}
+
+		prevPos = pos
 	}
 
 	print("zeroes: ", zeroes)
